a2d5d8640b00c119357b3297b6f1715aff6ac9ac
Chaitanya134 <chaitanyagiri2@gmail.com>
2022-01-25 02:58:37 +0530

    Completed Hobbies Section

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 const heroNavLinks = document.querySelectorAll(".hero-nav-link");
 
-for (const heroNavLink of heroNavLinks) {
-    heroNavLink.addEventListener("click", function() {
-        document.querySelector(".hero-nav-link.active").classList.remove("active");
-        this.classList.add("active");
-    })
+function scrollToSection(section) {
+    const element = document.getElementById(section);
+    element.scrollIntoView({ behavior: "smooth" });
+    document.querySelector(".hero-nav-link.active").classList.remove("active");
+    document.getElementById(`${section}-nav-link`).classList.add("active");
 }
-
-// let currentHeadingIndex = 0;
-// const animationInterval = setInterval(() => {
-//     const heroHeadings = document.querySelectorAll(".hero-heading-animation");
-//     heroHeadings[currentHeadingIndex].style.display = "none";
-//     currentHeadingIndex++;
-//     if (currentHeadingIndex === 5) {
-//         currentHeadingIndex = 0;
-//     }
-//     heroHeadings[currentHeadingIndex].style.display = "inline-block";
-// }, 6000)
