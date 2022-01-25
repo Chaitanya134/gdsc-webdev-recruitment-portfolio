d28878cfe2d210b61d4f384ecd2d665a2a34dbc9
Chaitanya134 <chaitanyagiri2@gmail.com>
2022-01-25 18:37:33 +0530

    Completed Skills Section

--- script.js
+++ script.js
@@ -6,3 +6,62 @@ function scrollToSection(section) {
     document.querySelector(".hero-nav-link.active").classList.remove("active");
     document.getElementById(`${section}-nav-link`).classList.add("active");
 }
+
+const skills = {
+    HTML: {
+        percent: "90%",
+        icon: "fa fa-html5",
+        color: "rgb(255 67 34)"
+    },
+    CSS: {
+        percent: "80%",
+        icon: "fa fa-css3",
+        color: "rgb(0, 132, 255)"
+    },
+    JavaScript: {
+        percent: "90%",
+        icon: "fab fa-js",
+        color: "rgb(255, 217, 0)"
+    },
+    ReactJs: {
+        percent: "85%",
+        icon: "fab fa-react",
+        color: "#61dafb"
+    },
+    NodeJs: {
+        percent: "60%",
+        icon: "fab fa-node-js",
+        color: "#6DA15D"
+    },
+    Git: {
+        percent: "45%",
+        icon: "fab fa-git-alt",
+        color: "red"
+    },
+    Database: {
+        percent: "85%",
+        icon: "fas fa-database",
+        color: "darkgray"
+    }
+}
+
+function createSkills() {
+    const skillsContainer = document.getElementById("skills-container");
+    Object.keys(skills).map(skill => {
+        const { percent, icon, color } = skills[skill];
+        const skillsDiv = document.createElement("DIV");
+        skillsDiv.className = `skills-div skills-${skill.toLowerCase()}`;
+        skillsDiv.innerHTML = `
+            <div>
+                <i class="${icon}" style="color: ${color}"></i><span>${skill}</span>
+            </div>
+            <div class="progress-bar-container">
+                <div class="progress-bar" style="width: ${percent}; background: ${color}"></div>
+                <span class="progress-bar-percent">${percent}</span>
+            </div>
+            `
+        skillsContainer.appendChild(skillsDiv);
+    })
+}
+
+createSkills();
